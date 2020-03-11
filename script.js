fbd3e5666c2eaac172907a01ff6a12aad2c512a8
henriqueamorim <hacmelo@gmail.com>
2020-03-11 03:46:22 -0300

    Adição da lógica da batalha de monstrinhos

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
+// Pega os valores dos inputs do usuário,em seguida  calcula a diferença entre os ataques e as defesas dos monstrinhos. Caso o monstrinho 1 tenha um ataque maior que o monstrinho 2 e também tenha uma defesa igual ao ataque do mostrinho 2, ele ganha. Caso o monstrinho 2 tenha um ataque maior que o monstrinho 1 e também tenha uma defesa igual ao ataque do mostrinho 2, ele ganha. Caso contrário, dá empate.
+
 function batalha() {
 
-    var ataque1 = document.querySelector('#ataque1').value
-    var ataque2 = document.querySelector('#ataque2').value
-    var defesa1 = document.querySelector('#defesa1').value
-    var defesa2 = document.querySelector('#defesa2').value
+    var diferenca1 = document.querySelector('#ataque1').value - document.querySelector('#defesa2').value
+    var diferenca2 = document.querySelector('#ataque2').value - document.querySelector('#defesa1').value
 
-    console.log(ataque1, ataque2, defesa1, defesa2)
-
-    if (ataque1 == 1) {
-        window.alert("Monstrinho 1 venceu!")
-        location.reload()
-    } else if (ataque1 == 2) {
+    if (diferenca1 == 0 && diferenca2 != 0) {
         window.alert("Monstrinho 2 venceu!")
+        location.reload()
+    } else if (diferenca2 == 0 && diferenca1 != 0) {
+        window.alert("Monstrinho 1 venceu!")
         location.reload()
     } else {
         window.alert("Empatou!")
